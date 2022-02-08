ca3c178650d2472152456730aa58f2ff42ee5481
Ulrik Strid <ulrik.strid@outlook.com>
2022-02-08 21:01:27 +0100

    Correct documentation in userinfo

--- oidc/Userinfo.ml
+++ oidc/Userinfo.ml
@@ -1,8 +1,10 @@
-(**
+(*
  * Copyright 2022 Ulrik Strid. All rights reserved.
  * Use of this source code is governed by a BSD-style
  * license that can be found in the LICENSE file.
  *)
+
+(** Validate that sub returned from userinfo is the same as in the id_token *)
 let validate ~(jwt : Jose.Jwt.t) userinfo =
   let userinfo_json = Yojson.Safe.from_string userinfo in
   let userinfo_sub =
