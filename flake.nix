d97c20f0f0cd62647d4a2d3572ce9b0ca0a5890b
Ulrik Strid <ulrik.strid@outlook.com>
2022-03-11 09:09:39 +0100

    Add nix to CI and build docs there

--- flake.nix
+++ flake.nix
@@ -6,7 +6,7 @@
   
     flake-utils.url = "github:numtide/flake-utils";
 
-    ocaml-overlay.url = "github:anmonteiro/nix-overlays/ulrikstrid/unvendor-piaf-dream";
+    ocaml-overlay.url = "github:anmonteiro/nix-overlays";
     ocaml-overlay.inputs.nixpkgs.follows = "nixpkgs";
   };
 
@@ -39,6 +39,7 @@
             buildInputs = with pkgs; with ocamlPackages; [
                 ocaml-lsp
                 ocamlformat_0_20_1
+                ocamlformat-rpc_0_20_1
                 odoc
                 reenv
                 # dune-release
